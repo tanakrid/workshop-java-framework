3d03e7313f81e9fc9b73002230672f370cb0dc1d
tanakrid chanburi <tanakrid.c@ku.th>
2021-09-06 14:34:30 +0700

    add EmployeeController

--- demoapp/src/main/java/com/example/demoapp/DemoappApplication.java
+++ demoapp/src/main/java/com/example/demoapp/DemoappApplication.java
@@ -2,12 +2,20 @@ package com.example.demoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoappApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(DemoappApplication.class, args);
+//		System.out.println(context.getBeanDefinitionCount());
+//
+//		String[] names = context.getBeanDefinitionNames();
+//		Arrays.stream(names).forEach(System.out::println);
 	}
 
 
