f6e6d6bf5023b0f68976dbe0048c944a97c57198
tanakrid chanburi <tanakrid.c@ku.th>
2021-09-06 15:24:36 +0700

    use springtest with controller

---
+++ demoapp/src/test/java/com/example/demoapp/employee/EmployeeControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.demoapp.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // simulation start service
+class EmployeeControllerTest {
+
+    // similar postman
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void listEmployee() {
+
+        EmployeeResponse[] results = restTemplate.getForObject("/employees", EmployeeResponse[].class);
+
+        assertEquals(2, results.length);
+    }
+}
