ef40c30a418992e39998eb03fefad563ceba127f
tanakrid chanburi <tanakrid.c@ku.th>
2021-09-06 16:38:35 +0700

    Convert SpringBootTest to MVCTest

---
+++ demoapp/src/test/java/com/example/demoapp/employee/EmployeeControllerWvbMvcTest.java
@@ -0,0 +1,41 @@
+package com.example.demoapp.employee;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.ContentResultMatchers.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EmployeeController.class) // different with SpringBootTest that SBT will load all bean but MVCtest will load only bean that define
+class EmployeeControllerWvbMvcTest {
+
+    @Autowired
+    private MockMvc mvc; // look like templateSpringTest
+
+    @Test
+    void getEmployeeById() throws Exception {
+
+        int id = 1;
+        MvcResult result = mvc.perform(get("/employees/"+id)).andExpect(status().isOk()).andReturn();
+
+        // Converting response data from byte to object. It will give benefit when response too big because it will with byte
+        byte[] json = result.getResponse().getContentAsByteArray();
+        ObjectMapper mapper = new ObjectMapper();
+        EmployeeResponse response = mapper.readValue(json, EmployeeResponse.class);
+
+        assertEquals(id, response.getId());
+        assertEquals("Tanakrid", response.getName());
+
+    }
+
+    @Test
+    void listEmployee() {
+    }
+}
